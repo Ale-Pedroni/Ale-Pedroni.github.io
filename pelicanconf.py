e47c727d5556e75a8c94c1fd5fc49aa798a96430
Alessandro Pedroni <alessandro2.pedroni@mail.polimi.it>
2025-04-21 16:17:07 +0200

    uganda theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,17 @@
 AUTHOR = 'Plasma'
 SITENAME = 'Plasma Hut'
 SITEURL = "https://Ale-Pedroni.github.io"
+SITESUBTITLE = 'A minimalist brutalist website with Ugandan flag-inspired colors'
+FEED_DOMAIN = SITEURL
 
 PATH = "content"
+OUTPUT_PATH = 'output'
+THEME_STATIC_DIR = '/uganda_theme/static'
+THEME = './uganda_theme'
 
 TIMEZONE = 'Europe/Rome'
 
-DEFAULT_LANG = 'It'
+DEFAULT_LANG = 'En'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -29,7 +34,28 @@ SOCIAL = (
     ("Another social link", "#"),
 )
 
-DEFAULT_PAGINATION = 10
+# Pagination
+DEFAULT_PAGINATION = 5
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Display categories on menu and homepage
+DISPLAY_CATEGORIES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_HOMEPAGE = True
+
+# Display pages on menu
+DISPLAY_PAGES_ON_MENU = True
+
+# Enable Jinja2 extensions
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.loopcontrols', 'jinja2.ext.do']}
+
+# Custom settings for the theme
+CURRENTYEAR = 2025  # Update this each year
